148c79bf0f7c187348d26500547b56d17b9927c1
Gato4D <63893829+amasalgadog@users.noreply.github.com>
2024-07-24 18:19:52 -0400

    ADD 13_parsing_XML exercise

---
+++ python_for_everybody_specialization/using_python_to_access_web_data/13_parsing_XML.py
@@ -0,0 +1,32 @@
+import xml.etree.ElementTree as ET
+#   ET will work as an alias for the library imported
+
+input = '''<stuff>
+    <person>
+        <user x="2">
+            <id>001</id>
+            <name>Chuck</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Brent</name>
+        </user>
+    </person>
+</stuff>'''
+#   the triple quoted string is a multiple line string, and
+#   the input will be the data to parse
+
+stuff = ET.fromstring(input)
+#   fromstring will parse de string data in input in the form of a tree node
+lst = stuff.findall('person/user')
+#   findall will return a list of all the tags that follows the path given
+print('User count:', len(lst))
+
+for item in lst:
+    print()
+    print('Name', item.find('name').text)
+    #   find will return the node representing the 'name' tag and .text will specify that the information returned is located between the tags
+    print('Id', item.find('id').text)
+    #   same as previous
+    print('Attribute', item.get('x'))
+    #   .get will look for the attritute 'x' and return its value
