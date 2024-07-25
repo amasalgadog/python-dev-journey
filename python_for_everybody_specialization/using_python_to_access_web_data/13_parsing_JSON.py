d3844adadd2f6a7c72b5797c1b7e1f534fc6437e
Gato4D <63893829+amasalgadog@users.noreply.github.com>
2024-07-25 17:41:42 -0400

    ADD 13_parsing_JSON

---
+++ python_for_everybody_specialization/using_python_to_access_web_data/13_parsing_JSON.py
@@ -0,0 +1,38 @@
+import json
+
+input = '''
+{
+    "name" : "Chuck",
+    "phone" : {
+        "type" : "intl",
+        "number" : "+1 734 303 4456" },
+    "email" : {
+        "hide" : "yes" }    
+}'''
+
+info_dict = json.loads(input)
+#   loads() will parse the string 'input' into a dictionary, because 'input' format is one of dictionary
+
+print(info_dict)
+print(info_dict['name'])
+print(info_dict['email']['hide'])
+
+data ='''[
+{   "id" : "001",
+    "x" : "2",
+    "name" : "Chuck" },
+{   "id" : "009",
+    "x" : "7",
+    "name" : "Jeff" }   
+]'''
+
+info_lst = json.loads(data)
+#   loads() will parse the string 'data' into a list, because 'data' format is one of list
+
+print()
+print(info_lst)
+for item in info_lst:
+    print()
+    print(item['name'])
+    print(item['id'])
+    print(item['x'])
