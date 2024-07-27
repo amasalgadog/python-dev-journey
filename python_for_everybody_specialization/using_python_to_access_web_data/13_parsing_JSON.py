c18799597f9faeccb593a8e05d5b9cbcb15ffcf5
Gato4D <63893829+amasalgadog@users.noreply.github.com>
2024-07-27 11:59:44 -0400

    UPDATE 13_parsing_JSON - printing data for checking in

--- python_for_everybody_specialization/using_python_to_access_web_data/13_parsing_JSON.py
+++ python_for_everybody_specialization/using_python_to_access_web_data/13_parsing_JSON.py
@@ -13,7 +13,8 @@ input = '''
 info_dict = json.loads(input)
 #   loads() will parse the string 'input' into a dictionary, because 'input' format is one of dictionary
 
-print(info_dict)
+print('Parsing dictionary: ', info_dict)
+print()
 print(info_dict['name'])
 print(info_dict['email']['hide'])
 
@@ -30,7 +31,7 @@ info_lst = json.loads(data)
 #   loads() will parse the string 'data' into a list, because 'data' format is one of list
 
 print()
-print(info_lst)
+print('Parsing list: ', info_lst)
 for item in info_lst:
     print()
     print(item['name'])
