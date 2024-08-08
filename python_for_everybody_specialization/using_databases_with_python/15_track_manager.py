d145d8cbaeeff9e3431efd0cc285fdf1ae5d7275
Gato4D <63893829+amasalgadog@users.noreply.github.com>
2024-08-07 21:13:30 -0400

    UPDATE 15_track_manager - read tracks information from file and parse into database

--- python_for_everybody_specialization/using_databases_with_python/15_track_manager.py
+++ python_for_everybody_specialization/using_databases_with_python/15_track_manager.py
@@ -3,6 +3,7 @@
 Exercise to simulate a track-managing database
 UPDATE 0: create tables and insert data into them
 UPDATE 1: using join operator
+UPDATE 2: add file with track records
 
 '''
 
@@ -25,7 +26,7 @@ cur.execute('''
             genre_id INTEGER NOT NULL,
             FOREIGN KEY (album_id) REFERENCES Album (id), 
             FOREIGN KEY (genre_id) REFERENCES Genre (id), 
-            PRIMARY KEY (id AUTOINCREMENT)
+            PRIMARY KEY (id)
             )''')
 # PRIMARY KEY (<attribute> AUTOINCREMENT) 
 # declare <attribute> as an unique key that increase its number every time a record/value is added to the table
@@ -38,10 +39,10 @@ cur.execute('DROP TABLE IF EXISTS Album')
 cur.execute('''
             CREATE TABLE Album (
             id INTEGER NOT NULL,
-            title TEXT,
+            title TEXT UNIQUE,
             artist_id INTEGER NOT NULL,
             FOREIGN KEY (artist_id) REFERENCES Artist (id),
-            PRIMARY KEY (id AUTOINCREMENT)
+            PRIMARY KEY (id)
             )''')
 
 # create or redo Artist table
@@ -49,8 +50,8 @@ cur.execute('DROP TABLE IF EXISTS Artist')
 cur.execute('''
             CREATE TABLE Artist (
             id INTEGER NOT NULL, 
-            name TEXT, 
-            PRIMARY KEY(id AUTOINCREMENT)
+            name TEXT UNIQUE, 
+            PRIMARY KEY(id)
             )''')
 
 # create or redo Genre table
@@ -58,11 +59,47 @@ cur.execute('DROP TABLE IF EXISTS Genre')
 cur.execute('''
             CREATE TABLE Genre (
             id INTEGER NOT NULL,
-            name TEXT NOT NULL,
-            PRIMARY KEY (id AUTOINCREMENT)
+            name TEXT UNIQUE,
+            PRIMARY KEY (id)
             )''')
 
-# insert record/data into Genre table
+handle = open('using_databases_with_python/tracks.csv')
+
+for line in handle:
+    line = line.strip()
+    stuff = line.split(',')
+
+    if len(stuff) < 6: continue
+
+    track = stuff[0]
+    artist = stuff[1]
+    album = stuff[2]
+    count = stuff[3]
+    rating = stuff[4]
+    length = stuff[5]
+    genre = stuff[6]
+
+    # IGNORE works with UNIQUE constraints, preventig a value from duplicating
+    # if UNIQUE wasn't declared the query will just add the value again and the IGNORE operator will do nothing
+    cur.execute("INSERT OR IGNORE INTO Genre (name) VALUES ( ?)", (genre,))
+    cur.execute("SELECT id FROM Genre WHERE name=?", (genre, ))
+    genre_id = cur.fetchone()[0]
+    # because fetchone, fetchall and fetchmany return a list of data, an index must be given even if it's just one data
+    
+    cur.execute("INSERT OR IGNORE INTO Artist (name) VALUES ( ?)", (artist,))
+    cur.execute("SELECT id FROM Artist WHERE name=?", (artist,))
+    artist_id = cur.fetchone()[0]
+    
+    cur.execute("INSERT OR IGNORE INTO Album (title, artist_id) VALUES ( ?, ?)", (album,artist_id,))
+    cur.execute("SELECT id FROM Album WHERE title=?", (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute("INSERT OR REPLACE INTO Track (title, rating, len, count, album_id, genre_id) VALUES ( ?, ?, ?, ?, ?, ?)", (track, rating, length, count, album_id, genre_id))
+
+    conn.commit()
+
+
+'''# insert record/data into Genre table
 cur.execute("INSERT INTO Genre (name) VALUES ('Rock')")
 cur.execute("INSERT INTO Genre (name) VALUES ('R&B')")
 cur.execute("INSERT INTO Genre (name) VALUES ('Trance')")
@@ -110,7 +147,7 @@ cur.execute("SELECT Track.title, Album.title, Artist.name, Genre.name FROM Track
 result3 = cur.fetchall()
 # print(result3)
 for item in result3:
-    print(item)
+    print(item)'''
 
 # close cursor
 cur.close()
